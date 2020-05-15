172510a59350265f5eeae6551101006a9926f198
Subs <BytesWarning@gmail.com>
2020-05-15 23:09:30 +1200

    Issue #2 - add baidu as translation source

--- static/src/utils.js
+++ static/src/utils.js
@@ -1,3 +1,5 @@
+import md5 from 'md5';
+
 import secrets from "./secrets";
 
 export async function googleTranslate(kwargs) {
@@ -13,6 +15,25 @@ export async function googleTranslate(kwargs) {
     const response = await fetch(url);
     const data = await response.json();
     return data.data.translations[0].translatedText;
+}
+
+export async function baiduTranslate(kwargs) {
+    const salt = Math.random() * 2 ** 16;
+    const params = {
+        appid: secrets.baiduAppId,
+        salt: salt,
+        sign: md5(secrets.baiduAppId + kwargs.text + salt + secrets.baiduSecretKey),
+        q: kwargs.text,
+        from: kwargs.language === "en" ? "en" : "mao",
+        to: kwargs.language === "en" ? "mao" : "en",
+    };
+    let url = new URL(secrets.baiduUrl);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.trans_result[0].dst;
 }
 
 export async function googleDetect(text) {
